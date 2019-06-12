eccfaabf7b61afdd783e23b19b30ccc48f8e035b
vicky_LinYoung <292442545@qq.com>
2019-06-12 11:09:04 +0800

    no_comments_version

--- src/main/java/exam/PaginationHelper.java
+++ src/main/java/exam/PaginationHelper.java
@@ -6,9 +6,7 @@ import java.util.List;
 public class PaginationHelper<I> {
     private List<I> collection ;
     private int itemPerPage ;
-    //item num in total
     private int itemNum ;
-    //page num in total
     private int pagesNum;
 
 
@@ -16,42 +14,23 @@ public class PaginationHelper<I> {
         this.collection = collection;
         this.itemPerPage = itemsPerPage;
         this.itemNum = collection.size();
-        // low performance calculate pagesNum;
         this.pagesNum = (int) Math.ceil((float) collection.size() / (float) itemPerPage);
     }
 
-
     public int itemCount() { return itemNum; }
-
 
     public int pageCount() {
         return pagesNum;
     }
 
-    /**
-     * pageIndex(zero based index)
-     * @param pageIndex
-     * @return itemsInPage
-     */
     public int pageItemCount(int pageIndex) {
-        //legal check
         if (pageIndex >= pagesNum || pageIndex < 0) return -1;
-        //full item page
         else if ((pageIndex + 1) * itemPerPage <= itemNum) return itemPerPage;
-        //last page
         else return itemNum % ((pageIndex) * itemPerPage);
-
     }
 
-    /**
-     * itemIndex (zero based index)
-     * @param itemIndex
-     * @return iteminPage
-     */
     public int pageIndex(int itemIndex) {
-        //legal check
         if (itemIndex >= itemNum || itemIndex < 0) return -1;
-        //perform
         else return itemIndex / itemPerPage;
     }
 }
